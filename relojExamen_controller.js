e233f61faaf8eba9a1afec084093b605cb67a516
Victor Manuel Rivas Santos <vrivas@ujaen.es>
2016-04-27 14:57:48 +0200

    Style and controller

---
+++ relojExamen_controller.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var app = angular.module('rEApp', []);
+app.controller('rECtrl', function($scope) {
+    $scope.hora=function() {
+        return new Date();
+    };
+});
